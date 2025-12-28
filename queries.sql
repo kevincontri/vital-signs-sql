c36a0442b7ff79c903d809f2f1007ee10cd3899f
Kevin Contri <kevin.lucas.contri@gmail.com>
2025-12-28 18:55:18 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,15 +1,27 @@
--- Nome do paciente que apresentou maior frequência cardíaca e qual esse valor
--- Para mudar o sinal vital sendo consultado, basta colocá-lo dentro dos parêntesis da função MAX()
-SELECT pacientes.nome, MAX(fc)
-FROM sinais_vitais
-JOIN pacientes ON pacientes.id = sinais_vitais.id_paciente;
-
--- Nome do paciente que apresentou menor pressão arterial
-SELECT pacientes.nome, MIN(pa)
-FROM sinais_vitais
-JOIN pacientes ON pacientes.id = sinais_vitais.id_paciente;
-
 -- Todos os sinais dos pacientes com o nome deles
 SELECT pacientes.nome, pa, fc, fr, tax, sat
 FROM sinais_vitais
 JOIN pacientes ON pacientes.id = sinais_vitais.id_paciente;
+
+-- Nome do paciente que apresentou maior frequência cardíaca e qual esse valor
+SELECT pacientes.nome, MAX(fc)
+FROM sinais_vitais
+JOIN pacientes ON pacientes.id = sinais_vitais.id_paciente;
+
+-- Nome do paciente que apresentou hipertermia
+SELECT pacientes.nome, sinais_vitais.tax
+FROM sinais_vitais
+JOIN pacientes ON pacientes.id = sinais_vitais.id_paciente
+WHERE sinais_vitais.tax >= 37.8;
+
+-- Nome do paciente que apresentou bradicardia
+SELECT pacientes.nome, sinais_vitais.fc
+FROM sinais_vitais
+JOIN pacientes ON pacientes.id = sinais_vitais.id_paciente
+WHERE sinais_vitais.fc < 60;
+
+-- Nome do paciente que apresentou taquipneia
+SELECT pacientes.nome, sinais_vitais.fr
+FROM sinais_vitais
+JOIN pacientes ON pacientes.id = sinais_vitais.id_paciente
+WHERE sinais_vitais.fr > 20;
